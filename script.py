f1b3760e97a8e595e7b56a16064e725b5bac95c2
Abhishek Mishra <abhishek@Abhisheks-MacBook-Pro.local>
2018-10-14 11:49:37 -0400

    cleaned up code

--- script.py
+++ script.py
@@ -20,58 +20,75 @@ df[df < 0] = 0
 x = np.array([i[0:781] for i in df.values])
 y = np.array([i[781] for i in pd.read_csv('inputs_final.csv').values])
 
+var_names = pd.read_csv('hackathon_els_variables.csv')
+var_names = dict(zip(
+	[i[0] for i in var_names.values],
+	[i[1] for i in var_names.values]))
+
 # feature selection - determine best features
 x_orig = x
 y_orig = y
-anova = SelectKBest(f_classif, k=3)
+anova = SelectKBest(f_classif, k=15)
 anova.fit(x_orig, y_orig)
-print("Best variables:", 
-	[df.columns.get_values()[i] for i in anova.get_support(True)])
+print("Best variables (no order):", 
+	*[var_names[df.columns.get_values()[i]] for i in anova.get_support(True)], sep="\n - ")
 
 # model selection
 
 # LDA
+print('-------------------------------------------------')
+print("Training LDA...")
 x_orig = x
 y_orig = y
 lda = LinearDiscriminantAnalysis()
 lda.fit(x_orig, y_orig)
 cs = cross_val_score(lda, x_orig, y_orig, cv=5)
-print("LDA:", cs.mean())
+print("Average LDA Accuracy:", '{:.2%}'.format(cs.mean()))
 
 # MLP
+print('-------------------------------------------------')
+print("Training MLP...")
 x_orig = x
 y_orig = y
 mlp = MLPClassifier(hidden_layer_sizes=(300,150))
 mlp.fit(x_orig, y_orig)
 cs = cross_val_score(mlp, x_orig, y_orig, cv=5)
-print("MLP:", cs.mean())
+print("Average MLP Accuracy:", '{:.2%}'.format(cs.mean()))
 
 # LDA with ANOVA
+print('-------------------------------------------------')
+print("Training LDA with ANOVA...")
 x_orig = x
 y_orig = y
 pipe = Pipeline([('anova', SelectKBest(f_classif, k=50)),
                 ('lda', LinearDiscriminantAnalysis())])
 pipe.fit(x_orig, y_orig)
 pipe.score(x_orig, y_orig)
-cs = cross_val_score(pipe, x_orig, y_orig, cv=5).mean()
-print("ANOVA LDA:", cs)
+cs = cross_val_score(pipe, x_orig, y_orig, cv=5)
+print("Average ANOVA LDA Accuracy:", '{:.2%}'.format(cs.mean()))
 
 # MLP with ANOVA
+print('-------------------------------------------------')
+print("Training MLP with ANOVA...")
 x_orig = x
 y_orig = y
 pipe = Pipeline([('anova', SelectKBest(f_classif, k=50)),
                 ('mlp', MLPClassifier(hidden_layer_sizes=(10,5)))])
 pipe.fit(x_orig, y_orig)
 pipe.score(x_orig, y_orig)
-cs = cross_val_score(pipe, x_orig, y_orig, cv=5).mean()
-print("ANOVA MLP:", cs)
+cs = cross_val_score(pipe, x_orig, y_orig, cv=5)
+print("Average ANOVA MLP Accuracy:", '{:.2%}'.format(cs.mean()))
 
 # K-Nearest Neighbors with ANOVA
+print('-------------------------------------------------')
+print("Training K-Nearest Neighbors with ANOVA...")
 x_orig = x
 y_orig = y
 pipe = Pipeline([('anova', SelectKBest(f_classif, k=15)),
                 ('knn', KNeighborsClassifier(n_neighbors = 300, 
                                              weights='distance'))])
 pipe.fit(x_orig, y_orig)
-cs = cross_val_score(pipe, x_orig, y_orig, cv=5).mean()
-print("ANOVA KNN:", cs)
+cs = cross_val_score(pipe, x_orig, y_orig, cv=5)
+print("Average ANOVA KNN Accuracy:", '{:.2%}'.format(cs.mean()))
+
+
